cd068ef5115c50e57267f09442739e916c6b49b0
Viktor Alm <viktoralm@pop-os.localdomain>
2020-03-09 12:59:21 +0100

    edit

--- tokenization.py
+++ tokenization.py
@@ -96,10 +96,7 @@ def preprocess_text(inputs, remove_space=True, lower=False):
       outputs = six.ensure_text(outputs, "latin-1")
 
   outputs = unicodedata.normalize("NFKD", outputs)
-  
-  #This removes åäö
-  #outputs = "".join([c for c in outputs if not unicodedata.combining(c)])
-  
+  outputs = "".join([c for c in outputs if not unicodedata.combining(c)])
   if lower:
     outputs = outputs.lower()
 
